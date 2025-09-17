db3b29f1f77f31abbd7a001feae777bcf6cf037e
LuisDaRod <90777076+LuisDaRod@users.noreply.github.com>
2025-09-17 13:12:12 +0200

    animaciones listas, cámara lista y el wey ya corre
    he puesto que para correr hay que mantener pulsado shift (cualquiera de los dos).
    
    También añadí un booleano que indica cuando está corriendo, que me imagino que será necesario luego

--- Assets/_3DStealthGame/scripts/PlayerController.cs
+++ Assets/_3DStealthGame/scripts/PlayerController.cs
@@ -6,9 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public InputAction MoveAction;
+    Animator m_Animator;
 
     public float walkSpeed = 1.0f;
+    public float runSpeed = 2.0f;
     public float turnSpeed = 20f;
+    public float currentSpeed;
+
+    bool isRunning;
 
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
@@ -18,6 +23,9 @@ public class PlayerMovement : MonoBehaviour
     {
         m_Rigidbody = GetComponent<Rigidbody>();
         MoveAction.Enable();
+        m_Animator = GetComponent<Animator>();
+
+        isRunning = false;
     }
 
     void FixedUpdate()
@@ -26,6 +34,9 @@ public class PlayerMovement : MonoBehaviour
 
         float horizontal = pos.x;
         float vertical = pos.y;
+        
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) { currentSpeed = runSpeed; isRunning = true; }
+        else { currentSpeed = walkSpeed; isRunning = false; }
 
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
@@ -33,7 +44,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
 
+        bool isWalking = !Mathf.Approximately(horizontal,0f) || !Mathf.Approximately(vertical,0f);
+        m_Animator.SetBool("isWalking", isWalking);
+
         m_Rigidbody.MoveRotation(m_Rotation);
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * currentSpeed * Time.deltaTime);
     }
 }
