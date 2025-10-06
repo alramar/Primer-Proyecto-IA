d65173b8a591205987a278af01c1e88560cf80c1
Eva María García Petrakova <102732975+ViiDost6@users.noreply.github.com>
2025-10-06 16:19:04 +0200

    Nodos

--- Assets/_3DStealthGame/scripts/Espectro/Espectro.cs
+++ Assets/_3DStealthGame/scripts/Espectro/Espectro.cs
@@ -7,6 +7,7 @@ public class Espectro : Enemy
     [Header("Espectro Specific Settings")]
     public List<Object> objects_to_possess;
     public float possession_range = 5.0f;
+    public Transform player;
     public new void Start()
     {
 
@@ -18,18 +19,11 @@ public class Espectro : Enemy
 
     }
 
-    private void SetSpeed(float newSpeed)
+    public void Possess()
     {
-        speed = newSpeed;
-    }
+        // Uses A* Pathfinding to move to the nearest object in objects_to_possess within possession_range
+        // or goes close to the object nearest to the player jumping between objects
 
-    private void SetRotationSpeed(float newRotationSpeed)
-    {
-        rotation_speed = newRotationSpeed;
-    }
-    
-    private void SetDetectionRange(float newDetectionRange)
-    {
-        detection_range = newDetectionRange;
+
     }
 }
---
+++ Assets/_3DStealthGame/scripts/Node/Node.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [Header("Node Settings")]
+    private float distanceToPlayer;
+    public Transform player;
+    public bool isObject = false;
+    public List<Node> neighbors = new List<Node>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate distance to player
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+    }
+}
